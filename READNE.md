a8ebb10f8801e285bb8c259029b1d9cfb8256b27
Yurii Skrynnykov <truef1s7@gmail.com>
2018-03-19 15:48:59 +0200

    initial commit

---
+++ READNE.md
@@ -0,0 +1,1 @@
+Here is collection of different tools I've wrote for daily use.
---
+++ xchange.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import argparse
+from os import getenv
+
+import requests
+
+
+exchangerate_error_codes = {
+        'unknown-code': "Don't support either of the supplied currency codes",
+        'invalid-key': "Key isn't active or doesn't exist",
+        'malformed-request': "Malformed request",
+        'quota-reached': "Your account runs out of request quota"
+        }
+
+
+def get_rate(request):
+    rate = requests.get(request)
+
+    if rate.status_code != 200:
+        raise Exception(
+            'HTTP Exception occured, status code: {}'.format(rate.status_code))
+
+    data = rate.json()
+
+    if 'error' in data.keys():
+        print(exchangerate_error_codes[data['error']])
+        exit(1)
+
+    return data['rate']
+
+
+def make_request(args):
+    return '/'.join(['https://v3.exchangerate-api.com/pair', args.key,
+                    args.base.upper(), args.to.upper()])
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('base', help='base currency')
+    parser.add_argument('to', help='convert currency')
+    parser.add_argument('-k', '--key', default=getenv('ER_KEY', ''),
+                        help='your exchangerate-api API key')
+
+    parsed_args = parser.parse_args()
+    if parsed_args.key == '':
+        print('Please, set either ER_KEY env variable, or put '
+              'exchangerate-api API key explicitly using --key argument')
+        exit(1)
+
+    return parsed_args
+
+
+if __name__ == '__main__':
+    args = parse()
+    rate = get_rate(make_request(args))
+
+    print(rate)
